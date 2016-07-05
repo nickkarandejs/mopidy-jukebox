0762a2eccef42b01e6f6ea628c2fdb52ff16c3a2
Tom Parker <palfrey@tevp.net>
2016-07-05 12:06:38 +0100

    Rewrite package install as one block

--- cookbooks/mopidy-jukebox/recipes/default.rb
+++ cookbooks/mopidy-jukebox/recipes/default.rb
@@ -3,10 +3,8 @@
 
 include_recipe 'build-essential::default'
 
-%w{libsqlite3-dev libffi-dev gstreamer1.0-fluendo-mp3 gstreamer1.0-plugins-bad gstreamer1.0-alsa gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly libssl-dev gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 python-dev python-gi cron git}.each do |pkg|
-	package pkg do
-		action :install
-	end
+package %w{libsqlite3-dev libffi-dev gstreamer1.0-fluendo-mp3 gstreamer1.0-plugins-bad gstreamer1.0-alsa gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly libssl-dev gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 python-dev python-gi cron git} do
+	action :install
 end
 
 include_recipe "python::pip"
