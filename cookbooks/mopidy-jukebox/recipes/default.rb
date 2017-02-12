83035727b9245231f018fb482e2ccf9b742e1505
Tom Parker <palfrey@tevp.net>
2017-02-12 17:59:30 +0000

    Make sure rsyslog is installed first

--- cookbooks/mopidy-jukebox/recipes/default.rb
+++ cookbooks/mopidy-jukebox/recipes/default.rb
@@ -3,7 +3,7 @@
 
 include_recipe 'build-essential::default'
 
-%w{libsqlite3-dev libffi-dev gstreamer1.0-fluendo-mp3 gstreamer1.0-plugins-bad gstreamer1.0-alsa gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly libssl-dev gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 python-dev python-gi cron git}.each do |pkg|
+%w{libsqlite3-dev libffi-dev gstreamer1.0-fluendo-mp3 gstreamer1.0-plugins-bad gstreamer1.0-alsa gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly libssl-dev gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 python-dev python-gi cron git rsyslog}.each do |pkg|
 	package pkg do
 		action :install
 	end
