b87fe2b3d281a027375a012392c7d39d6554e678
Tom Parker <tom.parker@oliverwyman.com>
2017-06-28 14:43:58 +0100

    Set allowed origins explicitly to Raspbian and Pi foundation

--- cookbooks/mopidy-jukebox/attributes/default.rb
+++ cookbooks/mopidy-jukebox/attributes/default.rb
@@ -1,2 +1,6 @@
 override['python']['setuptools_version'] = '20.1.1'
-override['unattended-upgrades']['allowed_origins'] = ['Debian', 'Raspbian']
+override['unattended-upgrades']['allowed_origins'] = {
+  'security' => true,
+  'Raspbian' => true,
+  'Raspberry Pi Foundation' => true,
+}
